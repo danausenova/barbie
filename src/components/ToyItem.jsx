a92e50befb2e7d0e095a8dddb2bb47c6884d6809
Altyw <koshokov996@gmail.com>
2023-07-27 16:32:18 +0600

    fixed bags

--- src/components/ToyItem.jsx
+++ src/components/ToyItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
