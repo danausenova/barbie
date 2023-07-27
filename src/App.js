4ff93c8c37e86bdec33b6d41c6ab959c40d85f83
JakaOmurzakova <angelina_18_89@mail.ru>
2023-07-27 14:44:35 +0600

    footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import MainRoutes from "./routes/MainRoutes";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function App() {
   return (
